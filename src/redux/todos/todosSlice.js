05b912c0cedc3f2cc7ce75745e2e56a4207ea648
Ismail Karaalioglu <ismailkaraalioglu@yandex.com>
2022-09-03 16:50:41 +0300

    added redux and redux toolkit

---
+++ src/redux/todos/todosSlice.js
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const todosSlice = createSlice({
+  name: "todos",
+  initialState: {},
+  reducers: {},
+  extraReducers: {},
+});
+
+export default todosSlice.reducer;
