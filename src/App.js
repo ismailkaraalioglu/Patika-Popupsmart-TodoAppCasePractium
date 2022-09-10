f6775ea8dcea872bd980d05ead192593a70ab44c
Ismail Karaalioglu <ismailkaraalioglu@yandex.com>
2022-09-10 18:42:04 +0300

    Page404 page created

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Page404 from "./pages/Page404";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
       <Route path="/login" element={<Login />} />
+      <Route path="*" element={<Page404 />} />
     </Routes>
   );
 }
